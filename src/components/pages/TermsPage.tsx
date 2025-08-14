b9600a9089d3e1eda6ee45b50c9c233b9e3e316e
WebCraft-System <factsofukraine@gmail.com>
2025-08-14 10:54:53 +0200

    Updated trace

--- src/components/pages/TermsPage.tsx
+++ src/components/pages/TermsPage.tsx
@@ -1,4 +1,11 @@
-@@ .. @@
-+'use client'
-+
- import React from 'react';
+'use client';
+
+import React from 'react';
+
+export default function TermsPage() {
+  return (
+    <div>
+      <h1>Terms</h1>
+    </div>
+  );
+}
