909734fac3cd7ec92972d2d0191ed491796c2bc4
WebCraft-System <factsofukraine@gmail.com>
2025-08-14 10:53:39 +0200

    Updated ServicesPage.tsx

--- src/components/pages/ServicesPage.tsx
+++ src/components/pages/ServicesPage.tsx
@@ -1,4 +1,11 @@
-@@ .. @@
-+'use client'
-+
- import React from 'react';
+'use client';
+
+import React from 'react';
+
+export default function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+    </div>
+  );
+}
